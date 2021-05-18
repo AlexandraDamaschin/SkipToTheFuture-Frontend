ea9102340b57eb6c28b43b149c85a1385911a9ec
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-05-18 16:05:10 +0300

    Get data from back4app api

---
+++ lib/constanta.dart
@@ -0,0 +1,4 @@
+
+String kParseApplicationId= "wA4gu7suNp356oAUUvMBizSl8yvTZZvvp7SqkbiF";
+String kParseClientKey = "mQQmzV7QXFazuz6hWeVmLxgPm5iuqakHGqn1qKuA";
+String kParseApiUrl = "https://parseapi.back4app.com/graphql";
