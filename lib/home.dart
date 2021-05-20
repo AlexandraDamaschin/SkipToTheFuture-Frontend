b88551364fbdc050e8d5dda8bb9e87b67e983a73
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2021-05-20 19:45:49 +0300

    Register Login  (#1)
    * Add registration screen
    
    * User registration
    
    * Create pagination for register and login
    
    * Redirect to register with back button included
    
    * Implement login
    
    * Logout user
    
    * Redirect user to home screen when it's logged in

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final controllerEmail = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: const Text('Welcome, you\'re logged in',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'dart:ui';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:skip_to_the_future_app/login.dart';
+import 'package:skip_to_the_future_app/register.dart';
+
 import 'constants.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final keyApplicationId = kParseApplicationId;
+  final keyClientKey = kParseClientKey;
+  final keyParseServerUrl = kParseApiUrl;
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
+  runApp(MaterialApp(
+    title: 'Flutter SignUp',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-    @override
-  Widget build(BuildContext context) {
-    final HttpLink httpLink = HttpLink(
-      uri: kParseApiUrl,
-      headers: {
-        'X-Parse-Application-Id': kParseApplicationId,
-        'X-Parse-Client-Key': kParseClientKey
-      },
-    );
-
-    ValueNotifier<GraphQLClient> client = ValueNotifier(
-      GraphQLClient(
-        cache: OptimisticCache(dataIdFromObject: typenameDataIdFromObject),
-        link: httpLink,
-      ),
-    );
-
-    return MaterialApp(
-      home: GraphQLProvider(
-        child: MyHomePage(),
-        client: client,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String name;
-  String saveFormat;
-  String objectId;
-
-  String query = '''
-    query {
-      flutterGraphQL_ProgrammingLanguage(id:"HM6YKfylDS") {
-        id
-        name
-        stronglyTyped
-      }
-    }
-  ''';
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Parsing data using GraphQL',
-          ),
-        ),
-        body: Query(
-          options: QueryOptions(
-            documentNode: gql(query),
-          ),
-          builder: (
-            QueryResult result, {
-            Refetch refetch,
-            FetchMore fetchMore,
-          }) {
-            print(result.exception);
-            if (result.data == null) {
-              return Center(
-                  child: Text(
-                "Loading...",
-                style: TextStyle(fontSize: 20.0),
-              ));
-            } else {
-              return ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(result.data["flutterGraphQL_ProgrammingLanguage"]['name']),
-                    trailing: Text(result.data["flutterGraphQL_ProgrammingLanguage"]['stronglyTyped']
-                        ? "Strongly Typed"
-                        : "Weekly Typed"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                child: Text('Register'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
                   );
                 },
-                itemCount: 1,
-              );
-            }
-          },
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
