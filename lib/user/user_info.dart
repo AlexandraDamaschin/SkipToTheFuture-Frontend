735f4a05dc581c00c2121769c644f591a379e316
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-06-03 07:00:25 +0300

    Create new user profile

--- lib/user/user_info.dart
+++ lib/user/user_info.dart
@@ -72,39 +72,264 @@ class _UserInfoPageState extends State<UserInfoPage> {
                       );
                     }
 
-                    return SingleChildScrollView(
-                        padding: const EdgeInsets.all(8.0),
-                        physics: ScrollPhysics(),
+                    final user = snapshot.data[3] as ParseUser;
+                    final userVerified = user.emailVerified ?? false;
+
+                    return SafeArea(
                         child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ListView.builder(
-                                  shrinkWrap: true,
-                                  padding: EdgeInsets.only(top: 10.0),
-                                  itemCount: snapshot.data.length,
-                                  itemBuilder: (context, index) {
-                                    final user =
-                                        snapshot.data[index] as ParseUser;
-                                    final userVerified =
-                                        user.emailVerified ?? false;
-                                    return ListTile(
-                                      title: Text(
-                                          'Username: ${user.username} - Verified: ${userVerified.toString()}'),
-                                      subtitle: Text(user.createdAt.toString()),
-                                    );
-                                  }),
-                              SizedBox(
-                                height: 16,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image:
+                                      NetworkImage("add you image URL here "),
+                                  fit: BoxFit.cover)),
+                          child: Container(
+                            width: double.infinity,
+                            height: 200,
+                            child: Container(
+                              alignment: Alignment(0.0, 2.5),
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "Add you profile DP image URL here "),
+                                radius: 60.0,
                               ),
-                              Container(
-                                height: 50,
-                                child: ElevatedButton(
-                                  child: const Text('Logout'),
-                                  onPressed: () => doUserLogout(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 60,
+                        ),
+                        Text(
+                          "${user.username}",
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blueGrey,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Sibiu",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.black45,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${user.emailAddress}",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.black45,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Card(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 8.0),
+                            elevation: 2.0,
+                            child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 12, horizontal: 30),
+                                child: Text(
+                                  "Skill Sets",
+                                  style: TextStyle(
+                                      letterSpacing: 2.0,
+                                      fontWeight: FontWeight.w300),
+                                ))),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "App Developer || Digital Marketer",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.black45,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        Card(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "Project",
+                                        style: TextStyle(
+                                            color: Colors.blueAccent,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                        "15",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w300),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "Followers",
+                                        style: TextStyle(
+                                            color: Colors.blueAccent,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                        "2000",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w300),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              RaisedButton(
+                                onPressed: () {},
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0),
+                                ),
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                        colors: [
+                                          Colors.pink,
+                                          Colors.redAccent
+                                        ]),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  child: Container(
+                                    constraints: BoxConstraints(
+                                      maxWidth: 100.0,
+                                      maxHeight: 40.0,
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Contact me",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12.0,
+                                          letterSpacing: 2.0,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ]));
+                              RaisedButton(
+                                onPressed: () {},
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0),
+                                ),
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                        colors: [
+                                          Colors.pink,
+                                          Colors.redAccent
+                                        ]),
+                                    borderRadius: BorderRadius.circular(80.0),
+                                  ),
+                                  child: Container(
+                                    constraints: BoxConstraints(
+                                      maxWidth: 100.0,
+                                      maxHeight: 40.0,
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Portfolio",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12.0,
+                                          letterSpacing: 2.0,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 50,
+                              )
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              RaisedButton(
+                                onPressed: () => doUserLogout(),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(80.0),
+                                ),
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                        colors: [
+                                          Colors.pink,
+                                          Colors.redAccent
+                                        ]),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  child: Container(
+                                    constraints: BoxConstraints(
+                                      maxWidth: 100.0,
+                                      maxHeight: 40.0,
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Logout",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12.0,
+                                          letterSpacing: 2.0,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]),
+                      ],
+                    ));
                   }
               }
             }));
